aad157049c92c8fe37aa267ff69e63b9065c7dc2
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-09-06 18:30:33 +0800

    MODULE 1: BG IMAGE ADDED
    ADDED BACKGROUND IMAGE TO SIGN-IN & FORGOT PASSWORD PAGES

--- src/views/auth/ForgotPassword.jsx
+++ src/views/auth/ForgotPassword.jsx
@@ -12,10 +12,30 @@ import Col from "react-bootstrap/Col";
 import "./Auth.css";
 
 const PageContainer = styled.div`
-  background-color: #134074;
+  background-image: url("https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80");
+  background-size: cover;
+  background-position: center center;
   width: 100%;
-  min-height: 100vh;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
+
+// const PageContainer = styled.div`
+//   background-color: #134074;
+//   background-image: url("https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80");
+//   background-size: cover;
+//   background-position: center center;
+//   width: 100%;
+//   min-height: 100vh;
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
 function ForgotPassword() {
   const [validated, setValidated] = useState(false);
