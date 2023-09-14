f6a0dcf3a633671c4f6719f4393361390d324cfa
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-09-14 12:18:54 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 import MainLayout from "./views/layouts/MainLayout";
-import NoPage from "./views/auth/NoPage";
 
 // IMPORT FROM AUTH FOLDER
-import "./assets/Fonts.css";
-import "./App.css";
-
-// import ForgotPassword from "./views/auth/ForgotPassword";
 import SignIn from "./views/auth/SignIn";
 import NoPage from "./views/auth/NoPage";
+// IMPORT FROM SAHABAT FOLDER
+import SearchSahabat from "./views/sahabat/Search";
+import ResultSahabat from "./views/sahabat/SearchResult";
 
-//IMPORT FROM LAPORAN FOLDER
-import SearchTf01 from "./views/laporan/jadual-tf01/Search";
-import ResultTf01 from "./views/laporan/jadual-tf01/SearchResult";
-
-import SearchTf01ByCawangan from "./views/laporan/jadual-tf01-cawangan/Search";
-import ResultTf01ByCawangan from "./views/laporan/jadual-tf01-cawangan/SearchResult";
-import SearchTf02 from "./views/laporan/jadual-tf02/Search";
-import ResultTf02 from "./views/laporan/jadual-tf02/SearchResult";
-
-// LAPORAN
+// IMPORT FROM LAPORAN FOLDER
 import IndexLaporan from "./views/laporan/Index";
 import ShowProfilSahabat from "./views/laporan/profil-sahabat/Show";
 import ShowProfilSahabatTerperinci from "./views/laporan/profil-sahabat-terperinci/Show";
@@ -32,9 +21,11 @@ import ResultTf01ByCawangan from "./views/laporan/jadual-tf01-cawangan/SearchRes
 import SearchTf02 from "./views/laporan/jadual-tf02/Search";
 import ResultTf02 from "./views/laporan/jadual-tf02/SearchResult";
 
-// SELENGGARA
-import IndexSelenggara from "./views/selenggara/Index";
+// IMPORT FROM SELENGGARA FOLDER
+import IndexSelenggara from "./views/selenggara/kod-inflow-outflow/Index";
 import IndexKodInflowOutflow from "./views/selenggara/kod-inflow-outflow/Index";
+import IndexKodInflow from "./views/selenggara/kod-inflow-outflow/kod-inflow/Index";
+import IndexKodOutflow from "./views/selenggara/kod-inflow-outflow/kod-outflow/Index";
 import Dimensi from "./views/selenggara/Dimensi";
 import Hubungan from "./views/selenggara/Hubungan";
 import PinjamanAktiviti from "./views/selenggara/PinjamanAktiviti";
@@ -53,13 +44,12 @@ function App() {
             <Route path="result-sahabat" element={<ResultSahabat />} />
 
             {/* LAPORAN PATH */}
+            <Route path="laporan" element={<IndexLaporan />} />
             <Route path="profil-sahabat" element={<ShowProfilSahabat />} />
             <Route
               path="profil-sahabat-terperinci"
               element={<ShowProfilSahabatTerperinci />}
             />
-
-            <Route path="laporan" element={<IndexLaporan />} />
             <Route path="search-tf01" element={<SearchTf01 />} />
             <Route path="result-tf01" element={<ResultTf01 />} />
             <Route
@@ -70,7 +60,6 @@ function App() {
               path="result-tf01-cawangan"
               element={<ResultTf01ByCawangan />}
             />
-
             <Route path="search-tf02" element={<SearchTf02 />} />
             <Route path="result-tf02" element={<ResultTf02 />} />
 
@@ -80,6 +69,9 @@ function App() {
               path="kod-inflow-outflow"
               element={<IndexKodInflowOutflow />}
             />
+            <Route path="kod-inflow" element={<IndexKodInflow />} />
+            <Route path="kodoutflow" element={<IndexKodOutflow />} />
+
             <Route path="dimensi" element={<Dimensi />} />
             <Route path="hubungan" element={<Hubungan />} />
             <Route path="pinjaman-aktiviti" element={<PinjamanAktiviti />} />
