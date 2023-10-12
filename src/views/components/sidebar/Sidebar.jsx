3d9adc0e7316f8caa437b4b19e8332791789e924
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-10-12 16:56:31 +0800

    Sidebar CSS and animation
    A WIP

--- src/views/components/sidebar/Sidebar.jsx
+++ src/views/components/sidebar/Sidebar.jsx
@@ -1,22 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import SidebarMenu from "./SidebarMenu"
-import './Sidebar.css'
+import SidebarMenu from "./SidebarMenu";
+import "./Sidebar.css";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
-import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroup from "react-bootstrap/ListGroup";
+import { useState } from "react";
 
 function Sidebar() {
-  return(
-    <div className="sidebar">
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className={`sidebar ${isSidebarOpen ? "open" : "shrink"}`}>
+      <button onClick={toggleSidebar} className="toggle-button">
+        {isSidebarOpen ? <FaArrowCircleLeft /> : <FaArrowAltCircleRight />}
+      </button>
       <ListGroup variant="flush">
         {SidebarMenu.map((item, index) => (
           <ListGroup.Item key={index}>
-            <Link to={item.path}>{item.icon}{item.title}</Link>
+            <Link to={item.path}>
+              {item.icon}
+              {isSidebarOpen && item.title}
+            </Link>
           </ListGroup.Item>
         ))}
       </ListGroup>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
