df8a82ad5150552af68b2cb106569f9ca8870ac4
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-10-13 11:55:11 +0800

    Add toggle button to the sidebar

--- src/views/components/sidebar/Sidebar.jsx
+++ src/views/components/sidebar/Sidebar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 import SidebarMenu from "./SidebarMenu";
 import "./Sidebar.css";
-import { FaArrowCircleLeft } from "react-icons/fa";
-import { FaArrowAltCircleRight } from "react-icons/fa";
+import { TfiArrowCircleLeft } from "react-icons/tfi";
+import { TfiArrowCircleRight } from "react-icons/tfi";
 
 import ListGroup from "react-bootstrap/ListGroup";
 import { useState } from "react";
@@ -17,9 +17,15 @@ function Sidebar() {
 
   return (
     <div className={`sidebar ${isSidebarOpen ? "open" : "shrink"}`}>
-      <button onClick={toggleSidebar} className="toggle-button">
-        {isSidebarOpen ? <FaArrowCircleLeft /> : <FaArrowAltCircleRight />}
-      </button>
+      <div className="sidebar-toggle-button">
+        <button onClick={toggleSidebar} className="toggle-button">
+          {isSidebarOpen ? (
+            <TfiArrowCircleLeft size={25} />
+          ) : (
+            <TfiArrowCircleRight size={25} />
+          )}
+        </button>
+      </div>
       <ListGroup variant="flush">
         {SidebarMenu.map((item, index) => (
           <ListGroup.Item key={index}>
