c298c36142fb1ba9780f1222de1d970816f86981
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-10-17 17:51:32 +0800

    Update Sidebar

--- src/views/components/sidebar/Sidebar.jsx
+++ src/views/components/sidebar/Sidebar.jsx
@@ -4,12 +4,13 @@ import SidebarMenu from "./SidebarMenu";
 import "./Sidebar.css";
 import { TfiArrowCircleLeft } from "react-icons/tfi";
 import { TfiArrowCircleRight } from "react-icons/tfi";
+import { BsPersonCircle } from "react-icons/bs";
 
 import ListGroup from "react-bootstrap/ListGroup";
 import { useState } from "react";
 
 function Sidebar() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -20,18 +21,29 @@ function Sidebar() {
       <div className="sidebar-toggle-button">
         <button onClick={toggleSidebar} className="toggle-button">
           {isSidebarOpen ? (
-            <TfiArrowCircleLeft size={25} />
+            <TfiArrowCircleLeft size={26} />
           ) : (
-            <TfiArrowCircleRight size={25} />
+            <TfiArrowCircleRight size={26} />
           )}
         </button>
       </div>
+      {isSidebarOpen && (
+        <div className="user-profile">
+          {/* Profil Pengguna */}
+          <BsPersonCircle className="userIcon" />
+          <h5>Nurul Aida Nazihah Binti Abdul Rashid</h5>
+          <h6>(Unit Sistem Teknologi Maklumat)</h6>
+        </div>
+      )}
+
       <ListGroup variant="flush">
         {SidebarMenu.map((item, index) => (
           <ListGroup.Item key={index}>
             <Link to={item.path}>
-              {item.icon}
-              {isSidebarOpen && item.title}
+              <span className="sidebar-icon">{item.icon}</span>
+              {isSidebarOpen && (
+                <span className="sidebar-title">{item.title}</span>
+              )}
             </Link>
           </ListGroup.Item>
         ))}
