ec742e16e6c936f77e323ade4c3360ea7866679a
azlinliana <azlinliana.adlan@gmail.com>
2023-12-25 12:36:24 +0800

    Backend search jadual TF02

--- src/views/laporan/jadual-tf02/Search.jsx
+++ src/views/laporan/jadual-tf02/Search.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { useForm, Controller } from "react-hook-form";
+import "../Laporan.css";
+import ErrorAlert from '../../components/sweet-alert/ErrorAlert';
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
@@ -6,8 +9,7 @@ import { Col } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import ResultTf02 from "./SearchResult";
-import "../Laporan.css";
-
+import axios from 'axios';
 
 function SearchTf02() {
   // ------- FE ---------
@@ -17,66 +19,85 @@ function SearchTf02() {
     setIsSearchResultJadualTf02Visible(!isSearchResultJadualTf02Visible);
   }
 
+    // Form validation
+    const { handleSubmit, control, reset, formState: { errors } } = useForm();
+
+    // ----------BE----------
+    const [cawanganOptions, setCawanganOptions] = useState([]);
+  
+    // Fetch cawangan
+    useEffect(() => {
+      const fetchCawangans = async () => {
+        try {
+          const response = await axios.get(`http://127.0.0.1:8000/api/selenggara/cawangan/display-cawangan`);
+  
+          if (Array.isArray(response.data) && response.data.length > 0) {
+            setCawanganOptions(response.data.map(cawangan => ({
+              value: cawangan.id,
+              label: cawangan.namaCawangan,
+            })));
+          } 
+          else {
+            ErrorAlert(response.data);
+          }
+        } 
+        catch (error) {
+          ErrorAlert(error);
+        }
+      };
+
+      fetchCawangans();
+    }, []);
+
   return (
     <>
       <div className="pageTitle">
         <h1>Jadual TF02</h1>
 
         <Breadcrumb>
-          <Breadcrumb.Item className="previousLink" href="#">
-            Senarai Laporan
-          </Breadcrumb.Item>
+          <Breadcrumb.Item className="previousLink" href="#">Senarai Laporan</Breadcrumb.Item>
           <Breadcrumb.Item active>Jadual TF02</Breadcrumb.Item>
         </Breadcrumb>
       </div>
 
       <div className="searchSection">
-        <div className="searchHeader">
-          <h2>Analisa Penghantaran Per RM - Bulanan</h2>
-        </div>
+        <div className="searchHeader"><h2>Analisa Penghantaran Per RM - Bulanan</h2></div>
 
         <div className="searchBarSection">
           <Container className="container-fluid searchBar">
             <Row>
               {/* Cawangan */}
               <Col xs={12} md={12}>
-                <div>
+              <div>
+                <Form.Group>
                   <Form.Label>Cawangan</Form.Label>
-                  <Form.Select>
-                    <option>-Sila Pilih-</option>
-                    <option value="GERIK">GERIK</option>
-                    <option value="IPOH">IPOH</option>
-                    <option value="KERIAN">KERIAN</option>
-                    <option value="KINTA">KINTA</option>
-                    <option value="KUALA KANGSAR">KUALA KANGSAR</option>
-                    <option value="LENGGONG">LENGGONG</option>
-                    <option value="MANJUNG">MANJUNG</option>
-                    <option value="MINI TANJUNG MALIM">
-                      MINI TANJUNG MALIM
-                    </option>
-                    <option value="SELAMA">SELAMA</option>
-                    <option value="SERI ISKANDAR">SERI ISKANDAR</option>
-                    <option value="TAIPING">TAIPING</option>
-                    <option value="SELAMA">TAPAH</option>
-                    <option value="SELAMA">TELUK INTAN</option>
-                  </Form.Select>
-                </div>
+                  <Controller
+                    id="cawanganId"
+                    name="cawanganId"
+                    control={control}
+                    defaultValue=""
+                    rules={{ required: "Cawangan diperlukan." }}
+                    render={({ field: { onChange } }) => (
+                      <Form.Select onChange={(e) => {setSelectedCawangan(e.target.value); onChange(e);}} defaultValue="">
+                        <option value="" disabled>--Pilih Cawangan--</option>
+                        {cawanganOptions.map((cawangan) => (
+                          <option key={cawangan.value} value={cawangan.value}>{cawangan.label}</option>
+                        ))}
+                      </Form.Select>  
+                    )}
+                  />
+                  {errors.cawanganId && (<small className="text-danger">{errors.cawanganId.message}</small>)}
+                </Form.Group>
+              </div>
               </Col>
             </Row>
-            <div className="cariBtnPlacement">
-              <Button className="cariBtn" onClick={handleSearchResultJadualTf02Visibility}>
-                Cari
-              </Button>{" "}
-            </div>
+
+            <div className="cariBtnPlacement"><Button className="cariBtn" onClick={handleSearchResultJadualTf02Visibility}>Cari</Button>{" "}</div>
           </Container>
 
-          <div className="searchResultContainer">
-            {isSearchResultJadualTf02Visible && <ResultTf02/>}
-          </div>
+          <div className="searchResultContainer">{isSearchResultJadualTf02Visible && <ResultTf02/>}</div>
 
-          <div className="kembaliBtnPlacement">
-            <Button className="kembaliBtn">Kembali</Button>{" "}
-          </div>
+          <div className="kembaliBtnPlacement"><Button className="kembaliBtn">Kembali</Button>{" "}</div>
         </div>
       </div>
     </>
