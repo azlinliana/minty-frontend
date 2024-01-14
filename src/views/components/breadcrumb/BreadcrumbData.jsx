ce3c1777699fb37c9aabe6f3d54c3f1631fbc864
azlinliana <azlinliana.adlan@gmail.com>
2024-01-14 18:09:49 +0800

    Unfinalized dynamic breadcrumb

---
+++ src/views/components/breadcrumb/BreadcrumbData.jsx
@@ -0,0 +1,17 @@
+const BreadcrumbData = [
+  {
+    path: "/search-sahabat",
+    element: "Carian Sahabat",
+  },
+  {
+    path: "/search-result-sahabat",
+    element: "Hasil Carian Sahabat",
+  },
+  {
+    path: "/tracking-inflow-outflow",
+    element: "Kemas Kini Tracking Sahabat",
+  },
+];
+
+
+export default BreadcrumbData;
