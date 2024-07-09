394282fc4d1067b3d57e47bdd297ffd0ccbd3d69
azlinliana <azlinliana.adlan@gmail.com>
2024-07-09 14:47:25 +0800

    Update edit isi rumah

--- src/views/sahabat/tracking-inflow-outflow/isi-rumah/Edit.jsx
+++ src/views/sahabat/tracking-inflow-outflow/isi-rumah/Edit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import SuccessAlert from "../../../components/sweet-alert/SuccessAlert";
 import ErrorAlert from "../../../components/sweet-alert/ErrorAlert";
@@ -7,13 +7,6 @@ import axiosCustom from "../../../../axios";
 
 function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
   // __________________________________ Frontend __________________________________
-  // Modal
-  const [isModalEditIsiRumah, setIsModalEditIsiRumah] = useState(false);
-  const openModalEditIsiRumah = () => setIsModalEditIsiRumah(true);
-  const closeModalEditIsiRumah = () => {
-    setIsModalEditIsiRumah(false);
-  };
-
   // Form validation
   const {
     register,
@@ -22,6 +15,64 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
     formState: { errors },
     reset,
   } = useForm();
+
+  // _____________________________ Frontend & Backend _____________________________
+  // Match data from zustand & backend
+  const findOptionId = (options, key, value) => {
+    const option = options.find((option) => option[key] === value);
+
+    return option ? option.id : "";
+  };
+
+  // Match data
+  const hubunganId = findOptionId(
+    hubunganOptions,
+    "kodHubungan",
+    isiRumahSahabat.hubungan
+  );
+
+  // Modal
+  const [isModalEditIsiRumah, setIsModalEditIsiRumah] = useState(false);
+
+  const openModalEditIsiRumah = () => setIsModalEditIsiRumah(true);
+
+  const closeModalEditIsiRumah = () => {
+    setIsModalEditIsiRumah(false);
+
+    // Reset previous form input
+    const resetFields = {
+      noKadPengenalanIsiRumah: isiRumahSahabat.noKadPengenalanIsiRumah,
+      namaIsiRumah: isiRumahSahabat.namaIsiRumah,
+      hubunganId: hubunganId,
+    };
+
+    reset(resetFields);
+  };
+
+  // Set default values when the edit isi rumah modal is opened
+  const [formData, setFormData] = useState({
+    noKadPengenalanIsiRumah: "",
+    namaIsiRumah: "",
+    hubunganId: "",
+  });
+
+  useEffect(() => {
+    // Populate form data
+    setValue(
+      "noKadPengenalanIsiRumah",
+      isiRumahSahabat.noKadPengenalanIsiRumah
+    );
+    setValue("namaIsiRumah", isiRumahSahabat.namaIsiRumah);
+    setValue("hubunganId", hubunganId);
+
+    // Set default values for formData
+    setFormData((prevData) => ({
+      ...prevData,
+      noKadPengenalanIsiRumah: isiRumahSahabat.noKadPengenalanIsiRumah,
+      namaIsiRumah: isiRumahSahabat.namaIsiRumah,
+      hubunganId,
+    }));
+  }, [isiRumahSahabat, setValue]);
 
   // ----------BE----------
   // Update isi rumah
@@ -46,6 +97,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
     <>
       <div>
         <span onClick={openModalEditIsiRumah}>Edit</span>{" "}
+
         <Modal
           show={isModalEditIsiRumah}
           onHide={closeModalEditIsiRumah}
@@ -58,6 +110,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
 
           <Form onReset={reset}>
             <Modal.Body>
+              {/* No. kad pengenalan isi rumah */}
               <Form.Group controlId="noKadPengenalanIsiRumah" className="mb-3">
                 <Form.Label className="form-label">
                   No. Kad Pengenalan
@@ -76,6 +129,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
                   aria-invalid={
                     errors.noKadPengenalanIsiRumah ? "true" : "false"
                   }
+                  placeholder="Masukkan no. kad pengenalan isi rumah"
                 />
 
                 {/* Validate required field */}
@@ -93,6 +147,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
                 )}
               </Form.Group>
 
+              {/* Nama isi rumah */}
               <Form.Group controlId="namaIsiRumah" className="mb-3">
                 <Form.Label className="form-label">Nama</Form.Label>
 
@@ -100,6 +155,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
                   type="text"
                   {...register("namaIsiRumah", { required: true })}
                   aria-invalid={errors.namaIsiRumah ? "true" : "false"}
+                  placeholder="Masukkan nama isi rumah"
                 />
                 {errors.namaIsiRumah?.type === "required" && (
                   <small className="text-danger">
@@ -108,6 +164,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
                 )}
               </Form.Group>
 
+              {/* Hubungan */}
               <Form.Group controlId="hubunganId" className="mb-3">
                 <Form.Label className="form-label">Hubungan</Form.Label>
 
@@ -140,6 +197,7 @@ function EditTrackingIsiRumah({ mingguId, isiRumahSahabat, hubunganOptions }) {
               <Button className="batal-btn" onClick={closeModalEditIsiRumah}>
                 Batal
               </Button>
+              
               <Button onClick={handleSubmit(updateIsiRumah)}>Simpan</Button>
             </Modal.Footer>
           </Form>
