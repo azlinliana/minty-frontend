6053bbc090864c98de5537bd0f2af6c1e183f118
azlinliana <azlinliana.adlan@gmail.com>
2025-03-12 10:59:22 +0800

    Change to EditLoan

---
+++ src/views/settings/loan/Edit.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Modal, Button, Form } from "react-bootstrap";
+import { useDimensiStore } from "../../../store/selenggara/dimensi-store";
+
+function EditLoan({ dimensi }) {
+  // __________________________________ Frontend __________________________________
+  // Modal
+  const [isModalEditLoan, setIsModalEditLoan] = useState(false);
+  const openModalEditLoan = () => setIsModalEditLoan(true);
+  const closeModalEditLoan = () => {
+    setIsModalEditLoan(false);
+  };
+
+  // Form validation
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  // ___________________________________ Backend __________________________________
+  // Set default values when the kemas kini dimensi modal is opened
+  // const [formData, setFormData] = useState({
+  //   kodDimensi: "",
+  //   keteranganDimensi: "",
+  //   statusDimensi: "",
+  // });
+
+  // useEffect(() => {
+  //   // Populate form data
+  //   setValue("kodDimensi", dimensi.kodDimensi);
+  //   setValue("keteranganDimensi", dimensi.keteranganDimensi);
+  //   setValue("statusDimensi", dimensi.statusDimensi);
+
+  //   // Set default values for formData
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     kodDimensi: dimensi.kodDimensi,
+  //     keteranganDimensi: dimensi.keteranganDimensi,
+  //     statusDimensi: dimensi.statusDimensi,
+  //   }));
+  // }, [dimensi, setValue]);
+
+  // Edit dimensi
+  // const { EditLoan } = useDimensiStore((state) => ({
+  //   EditLoan: state.EditLoan,
+  // }));
+
+  // Pass input & close modal
+  const handleEditLoan = (EditLoanData) => {
+    // EditLoan(dimensi.id, EditLoanData, closeModalEditLoan);
+  };
+
+  return (
+    <div>
+      <Button className="edit-btn" onClick={openModalEditLoan}>
+        Edit
+      </Button>{" "}
+      
+      <Modal
+        show={isModalEditLoan}
+        onHide={closeModalEditLoan}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Loan</Modal.Title>
+        </Modal.Header>
+
+        <Form onReset={reset}>
+          <Modal.Body>
+            <Form.Group controlId="kodDimensi" className="mb-3">
+              <Form.Label className="form-label">Loan Code</Form.Label>
+
+              <Form.Control
+                type="text"
+                // {...register("kodDimensi", { required: true })}
+                // aria-invalid={errors.kodDimensi ? "true" : "false"}
+                placeholder="Insert loan code"
+              />
+
+              {/* {errors.kodDimensi?.type === "required" && ( */}
+                <small className="text-danger">Loan code is required.</small>
+              {/* )} */}
+            </Form.Group>
+
+            <Form.Group controlId="keteranganDimensi" className="mb-3">
+              <Form.Label className="form-label">Loan Description</Form.Label>
+
+              <Form.Control
+                as="textarea"
+                // {...register("keteranganDimensi", { required: true })}
+                // aria-invalid={errors.keteranganDimensi ? "true" : "false"}
+                placeholder="Insert loan description"
+              />
+
+              {/* {errors.keteranganDimensi?.type === "required" && ( */}
+                <small className="text-danger">
+                  Loan description is required.
+                </small>
+              {/* )} */}
+            </Form.Group>
+
+            <Form.Group controlId="statusDimensi" className="mb-3">
+              <Form.Label className="form-label">Loan Status</Form.Label>
+
+              <Form.Control
+                as="select"
+                className="form-select"
+                // {...register("statusDimensi", { required: true })}
+                // aria-invalid={errors.statusDimensi ? "true" : "false"}
+                placeholder="Insert status dimensi"
+              >
+                <option value="" disabled>
+                  --Choose Loan Status--
+                </option>
+                <option value="ACTIVE">ACTIVE</option>
+                <option value="INACTIVE">INACTIVE</option>
+              </Form.Control>
+
+              {/* {errors.statusDimensi?.type === "required" && ( */}
+                <small className="text-danger">
+                  Loan status is required.
+                </small>
+              {/* )} */}
+            </Form.Group>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button className="batal-btn" onClick={closeModalEditLoan}>
+              Cancel
+            </Button>
+
+            <Button onClick={handleSubmit(handleEditLoan)}>Edit</Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </div>
+  );
+}
+
+export default EditLoan;
