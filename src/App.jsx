b0945cd83c845aa77bb94712450d9768337b1a52
azlinliana <azlinliana.adlan@gmail.com>
2025-04-07 10:07:49 +0800

    Update path and change name

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import MainLayout from "./views/layouts/MainLayout";
+
 import SignIn from "./views/auth/SignIn";
 import Dashboard from "./views/Dashboard";
 import SignUp from "./views/auth/SignUp";
+
 import SearchSahabat from "./views/sahabat/carian/SearchSahabat";
 import SearchResultSahabat from "./views/sahabat/carian/SearchResultSahabat";
 import TrackingInflowOutflow from "./views/sahabat/tracking-inflow-outflow/Tracking";
+
 import IndexReport from "./views/report/list-report/Index";
-import ShowProfilSahabat from "./views/report/profil-sahabat/show-result/Show";
-import ShowProfilSahabatTerperinci from "./views/report/profil-sahabat-terperinci/show-result/Show";
+import SearchResultCustomerFinancial from "./views/report/customer-profile/SearchResult";
+import ShowCustomerProfile from "./views/report/customer-profile/show-result/Show";
+import SearchResultDetailedCustomerFinancial from "./views/report/detailed-customer-profile/SearchResult";
+import ShowDetailedCustomerProfile from "./views/report/detailed-customer-profile/show-result/Show";
+
 import SearchReport1 from "./views/report/report-1/Search";
-import SearchResultReport1 from "./views/report/report-1/SearchResult";
 import SearchReport2 from "./views/report/report-2/Search";
-import SearchResultReport2 from "./views/report/report-2/SearchResult";
-import SearchProfilSahabat from "./views/report/profil-sahabat/Search";
-import SearchProfilSahabatTerperinci from "./views/report/profil-sahabat-terperinci/Search";
+
 import IndexSettings from "./views/settings/Index";
 import IndexInflowCode from "./views/settings/inflow-code/Index";
 import IndexOutflowCode from "./views/settings/outflow-code/Index";
 import IndexLoan from "./views/settings/loan/Index";
 import IndexRelationship from "./views/settings/relationship/Index";
+
 import IndexUser from "./views/user/Index";
 import IndexAdmin from "./views/user/admin/Index";
 import IndexSuperAdmin from "./views/user/super-admin/Index";
+
 import BadRequest from "./views/http_requests/BadRequest";
 import Unauthorized from "./views/http_requests/Unauthorized";
 import Forbidden from "./views/http_requests/Forbidden";
@@ -33,7 +40,6 @@ import TooManyRequests from "./views/http_requests/TooManyRequests";
 import InternalServer from "./views/http_requests/InternalServer";
 import ServiceUnavailable from "./views/http_requests/ServiceUnavailable";
 import UnknownError from "./views/http_requests/UnknownError";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -47,7 +53,7 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="dashboard" element={<Dashboard />} />
 
-            {/* Sahabat */}
+            {/* Customer */}
             <Route path="carian-sahabat" element={<SearchSahabat />} />
             <Route
               path="hasil-carian-sahabat"
@@ -58,34 +64,39 @@ function App() {
               element={<TrackingInflowOutflow />}
             />
 
-            {/* Laporan */}
-            <Route path="laporan" element={<IndexReport />} />
+            {/* Report */}
+            <Route path="report" element={<IndexReport />} />
+
             <Route
-              path="pembiayaan-sahabat"
-              element={<SearchProfilSahabat />}
-            />
-            <Route path="profil-sahabat" element={<ShowProfilSahabat />} />
-            ResultTf01ByCawangan
-            ResultTf01ByCawangan
-            <Route
-              path="pembiayaan-sahabat-terperinci"
-              element={<SearchProfilSahabatTerperinci />}
+              path="/customer-profile-list-financial"
+              element={<SearchResultCustomerFinancial />}
             />
             <Route
-              path="profil-sahabat-terperinci"
-              element={<ShowProfilSahabatTerperinci />}
+              path="/customer-profile-report"
+              element={<ShowCustomerProfile />}
             />
+
+            <Route
+              path="/detailed-customer-profile-list-financial"
+              element={<SearchResultDetailedCustomerFinancial />}
+            />
+            <Route
+              path="/detailed-customer-profile-report"
+              element={<ShowDetailedCustomerProfile />}
+            />
+
             <Route path="search-report-1" element={<SearchReport1 />} />
+            
             <Route path="search-report-2" element={<SearchReport2 />} />
 
-            {/* Selenggara */}
+            {/* Settings */}
             <Route path="settings" element={<IndexSettings />} />
             <Route path="inflow-code" element={<IndexInflowCode />} />
             <Route path="outflow-code" element={<IndexOutflowCode />} />
             <Route path="loan" element={<IndexLoan />} />
             <Route path="relationship" element={<IndexRelationship />} />
 
-            {/* Pengguna */}
+            {/* User */}
             <Route path="user-settings" element={<IndexUser />} />
             <Route path="admin-list" element={<IndexAdmin />} />
             <Route path="super-admin-list" element={<IndexSuperAdmin />} />
